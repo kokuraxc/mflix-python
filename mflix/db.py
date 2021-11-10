3f5ba15e23c48f463fe1fc0a707548955314f88f
Guo Zhengjin <guo.zhengjin@gmail.com>
2021-11-10 14:19:44 +0800

    Ticket: User Report

--- mflix/db.py
+++ mflix/db.py
@@ -417,7 +417,8 @@ def delete_comment(comment_id, user_email):
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
     # response = db.comments.delete_one({"_id": ObjectId(comment_id)})
-    response = db.comments.delete_one({"_id": ObjectId(comment_id), "email": user_email})
+    response = db.comments.delete_one(
+        {"_id": ObjectId(comment_id), "email": user_email})
     return response
 
 
@@ -607,9 +608,15 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {
+            '$sortByCount': '$email'
+        }, {
+            '$limit': 20
+        }
+    ]
 
-    rc = db.comments.read_concern  # you may want to change this read concern!
+    rc = ReadConcern(level="majority")
     comments = db.comments.with_options(read_concern=rc)
     result = comments.aggregate(pipeline)
     return list(result)
